f61991585474944cd18ea1fd8ffe20e5c013e47f
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-07-16 11:14:15 -0700

    add deployment route

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
 // use above URL to access javascript file
-app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 3045;
 app.listen(port, () =>
